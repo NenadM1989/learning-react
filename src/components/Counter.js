c933efb89639b1fb7829685263e1a44fafac1623
Nenad Mihajlovic <nenad.knele.mihajlovic89@gmail.com>
2024-04-11 15:32:57 +0200

    first commit

---
+++ src/components/Counter.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const STEP = 7;
+const MAX = 20;
+
+const Counter = () => {
+  const [count, setCount] = useState(13);
+
+  const increment = () => {
+    if (count + STEP > MAX) {
+      return;
+    }
+    setCount((prevCount) => prevCount + STEP);
+  };
+
+  const decrement = () => {
+    if (count - STEP < 0) {
+      return;
+    }
+    setCount((prevCount) => prevCount - STEP);
+  };
+
+  const textColor = count <= MAX / 2 ? "red" : "green";
+  return (
+    <div>
+      <button onClick={increment}>+</button>
+      <div style={{ color: textColor }}>{count}</div>
+      <button onClick={decrement}>-</button>
+    </div>
+  );
+};
+
+export default Counter;
